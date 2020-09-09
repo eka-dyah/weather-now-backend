915f01311ad8533f2593475215015d1e2162df62
eka-dyah <eka.dya1205@gmail.com>
2020-09-09 15:04:23 +0700

    change default city to Mountain View

--- util/getLocationByIp.js
+++ util/getLocationByIp.js
@@ -3,10 +3,11 @@ const HttpError = require("../models/http-error");
 const { url, key } = require("../config").weatherApi;
 
 const getLocationByIp = async (ipAddress, next) => {
-    // default city : London
+    // default city : Mountain view
+    console.log(ipAddress);
     let defaultCity;
     try {
-        defaultCity = await axios.get(`${url}search.json?${key}&q=london`);
+        defaultCity = await axios.get(`${url}search.json?${key}&q=Mountain%20view`);
     } catch (error) {
         return next(new HttpError("Something error", 502));
     }
@@ -14,21 +15,26 @@ const getLocationByIp = async (ipAddress, next) => {
 	let location;
 	try {
         location = await axios.get(`${url}ip.json?${key}&q=${ipAddress}`);
+        location = location.data;
 	} catch (err) {
 		location = defaultCity.data[0];
-	}
+    }
+    
+    if (!location) {
+        return next(new HttpError("Something error", 500));
+    }
 
-	if (Object.entries(location.data).length === 0) {
+	if (Object.entries(location).length === 0) {
 		return next(new HttpError("Something error", 500));
 	}
     
     const locationReturn = {
-        id: location.data.geoname_id,
-        name: `${location.data.city}, ${location.data.country_name}`,
-        region: location.data.region,
-        country: location.data.country_name,
-        lat: location.data.lat,
-        lon: location.data.lon
+        id: location.id || location.geoname_id,
+        name: location.name || `${location.city}, ${location.country_name}`,
+        region: location.region,
+        country: location.country_name,
+        lat: location.lat,
+        lon: location.lon
     }
 
 	return locationReturn;
